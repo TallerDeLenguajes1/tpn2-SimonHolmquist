a3acfba5e4f4ecedfe11edf655eb8efe48d23833
unknown <simon.holmquist@gmail.com>
2020-04-20 15:56:19 -0300

    completando punto 1)d)

---
+++ tp2_1_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#define N 4
+#define M 5
+
+using namespace std;
+
+int f,c;
+double mt[N][M];
+
+int main(){
+    double *p = &mt[0][0];
+    int k = 0;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            mt[i][j] = k++;
+        }
+    }
+    for(f = 0;f<N*M; f++)
+    {
+        cout<<*(p++)<<' ';
+        if((f+1)%M==0 && f<M*N) cout<<'\n';
+    }
+    return 0;
+}
